d9e198b41350616c35cf30b90c448e2a3fac9715
Chris Cajas <chris.cajas.m@gmail.com>
2023-05-15 23:53:00 -0500

    Fixed union alignment for cart header

--- src/cart.h
+++ src/cart.h
@@ -8,31 +8,36 @@
 typedef struct Cartridge_struct 
 {
     /* First 80 bytes after BIOS address */
-    union Header 
+    union
     {
-        uint8_t    entry[4];    /* 0x100 */
-        uint8_t    logo [0x30]; /* 0x104 */
-        union Title
+        struct 
         {
-            uint8_t   oldTitle[0x0f]; /* 0x134 */
-            uint8_t   cgb_flag;       /* 0x143 */
-        }
-        title;
-        uint16_t   licensee_new; /* 0x144 */
-        /* Cart technical info */
-        uint8_t    sgb_flag;     /* 0x146 */
-        uint8_t    cart_type;    /* 0x147 */
-        uint8_t    rom_size;     /* 0x148 */
-        uint8_t    ram_size;     /* 0x149 */
-        /* Other metadata */
-        uint8_t    dest_code;    /* 0x14A */
-        uint8_t    licensee_old; /* 0x14B */
-        uint8_t    mask_rom;     /* 0x14C */
-        
-        uint8_t    checksum_header;   /* 0x14D */
-        uint16_t   checksum_rom;      /* 0x14E */
-    }
-    header;
+            uint32_t   entry;       /* 0x100 */
+            uint8_t    logo [0x30]; /* 0x104 */
+            union
+            {
+                struct {
+                    char      oldTitle[0x0f]; /* 0x134 */
+                    uint8_t   cgb_flag;       /* 0x143 */
+                };
+                char   title[0x10];
+            };
+            uint16_t   licensee_new; /* 0x144 */
+            /* Cart technical info */
+            uint8_t    sgb_flag;     /* 0x146 */
+            uint8_t    cart_type;    /* 0x147 */
+            uint8_t    rom_size;     /* 0x148 */
+            uint8_t    ram_size;     /* 0x149 */
+            /* Other metadata */
+            uint8_t    dest_code;    /* 0x14A */
+            uint8_t    licensee_old; /* 0x14B */
+            uint8_t    mask_rom;     /* 0x14C */
+            
+            uint8_t    checksum_header;   /* 0x14D */
+            uint16_t   checksum_rom;      /* 0x14E */
+        };
+        uint8_t    header[GB_HEADER_SIZE];
+    };
 }
 Cartridge;
 
--- src/gb.c
+++ src/gb.c
@@ -22,8 +22,10 @@ void gb_load_cart (GameBoy * const gb)
         vc_init (&mmu->rom, size);
         vc_push_array (&mmu->rom, filebuf, size, 0);
 
+        memcpy (&gb->cart.header, filebuf + 0x100, GB_HEADER_SIZE);
+
         free(filebuf);
-        LOG_("GB: ROM loaded (%d bytes)\n", vc_size(&mmu->rom));
+        LOG_("GB: ROM loaded (%d bytes)\n", gb->cart.cart_type);//vc_size(&mmu->rom));
     }
 }
 
@@ -31,6 +33,13 @@ void gb_unload_cart (GameBoy * const gb)
 {
     vc_free (&gb->mmu.rom);
 
-    LOG_("GB: Header size: %d bytes\n", sizeof(gb->cart.header));
+    LOG_("GB: ROM loaded (%08x)\n", gb->cart.entry);
+    int i;
+    for (i = 0; i < 48; i++)
+    {
+        LOG_("%02x ", gb->cart.logo[i]);
+        if ((i & 0xf) == 0xf)
+            LOG_("\n");
+    }
     memset(&gb->cart.header, 0, GB_HEADER_SIZE);
 }
