78bfc124a670061a3c30f398234bb24ebeb729bc
Chris Cajas <chris.cajas.m@gmail.com>
2025-03-10 03:37:55 -0500

    Added variation of Simpletex LCD shader, new palettes

--- src/api/glfw/graphics.c
+++ src/api/glfw/graphics.c
@@ -21,24 +21,109 @@ const char * ppu_fs_source =
 "uniform vec2 screenSize;\n"
 
 "#define PI_2          3.1415926538 * 2\n"
-"#define SCREEN_SHADER passthru\n"
+"#define SCREEN_SHADER simpletex_lcd\n"
 
 "uniform float rr = 0.640000;\n"
 "uniform float rg = 0.660000;\n"
 "uniform float rb = 0.720000;\n"
 "uniform float colorP = 2.0;\n"
-"uniform float brightness = 1.4;\n"
+"uniform float brightness = 2.4;\n"
 
 "vec3 dot_matrix(vec3 color, vec3 tint)\n"
 "{\n"
 "    vec2 position = (TexCoords.xy);\n"
-"    float dotTint = 0.25;\n"
+"    float dotTint = 0.15;\n"
 "    if (color.r + color.g + color.b > 7.3) dotTint = 0;\n"
 "    //color = vec3(0.05) + (color * vec3(0.95)) - 0.05;\n"
-"    if (fract(position.x * screenSize.x) > 0.6) color = mix(color, vec3(1), dotTint * 1.25);"
-"    if (fract(position.y * screenSize.y) > 0.6) color = mix(color, vec3(1), dotTint);"
+"    //if (fract(position.x * screenSize.x) <= 0.25) color = mix(color, vec3(1), dotTint * 1.25);\n"
+"    //if (fract(position.y * screenSize.y) <= 0.25) color = mix(color, vec3(1), dotTint);\n"
+"    if (fract(position.x * screenSize.x) >= 0.75) color = mix(color, vec3(1), dotTint * 1.25);\n"
+"    if (fract(position.y * screenSize.y) >= 0.75) color = mix(color, vec3(1), dotTint);\n"
 "    color = vec3(pow(color.r, 1.2), pow(color.g, 1.2), pow(color.b, 1.2));\n"
 "    return color;\n"
+"}\n"
+
+"#define COMPAT_PRECISION mediump\n"
+
+// ### Magic Numbers...
+"#define GRID_INTENSITY 1.0\n"
+"#define GRID_WIDTH 0.8\n"
+"#define GRID_BIAS 0.7\n"
+"#define DARKEN_GRID 0.0\n"
+"#define DARKEN_COLOR 0.05\n"
+
+// Grid pattern
+// > Line weighting equation:
+//   y = a * (x^4 - b * x^6)
+"const COMPAT_PRECISION float LINE_WEIGHT_A = 48.0;\n"
+"const COMPAT_PRECISION float LINE_WEIGHT_B = 8.0 / 3.0;\n"
+
+// RGB -> Luminosity conversion
+// > Photometric/digital ITU BT.709
+"#define LUMA_R 0.2126\n"
+"#define LUMA_G 0.7152\n"
+"#define LUMA_B 0.0722\n"
+
+// Background texture size
+"#define BG_TEXTURE_SIZE 2048.0\n"
+"const COMPAT_PRECISION float INV_BG_TEXTURE_SIZE = 1.0 / BG_TEXTURE_SIZE;\n"
+
+"vec3 simpletex_lcd(vec3 color, vec3 tint)\n"
+"{\n"
+	// Get current texture coordinate
+"	COMPAT_PRECISION vec2 imgPixelCoord = (TexCoords.xy * screenSize.xy);\n"
+"	COMPAT_PRECISION vec2 imgCenterCoord = floor(imgPixelCoord.xy) + vec2(0.5, 0.5);\n"
+	
+	// Darken colors (if required...)
+"	color.rgb = pow(color.rgb, vec3(1.0 + DARKEN_COLOR));\n"
+	
+	// Generate grid pattern...
+"	COMPAT_PRECISION vec2 distFromCenter = abs(imgCenterCoord.xy - imgPixelCoord.xy);\n"
+	
+"	COMPAT_PRECISION float xSquared = max(distFromCenter.x, distFromCenter.y);\n"
+"	xSquared = xSquared * xSquared;\n"
+	
+"	COMPAT_PRECISION float xQuarted = xSquared * xSquared;\n"
+	
+	// > Line weighting equation:
+	//   y = 48 * (x^4 - (8/3) * x^6)
+"	COMPAT_PRECISION float lineWeight = LINE_WEIGHT_A * (xQuarted - (LINE_WEIGHT_B * xQuarted * xSquared));\n"
+	
+	// > Apply grid adjustments (phase 1)
+	//   - GRID_WIDTH:
+	//        1.0: Use raw lineWeight value
+	//        0.0: Use lineWeight ^ 2 (makes lines thinner - realistically, this is
+	//                                 the thinnest we can go before the grid effect
+	//                                 becomes pointless, particularly with 'high resolution'
+	//                                 systems like the GBA)
+	//   - GRID_INTENSITY:
+	//        1.0: Grid lines are white
+	//        0.0: Grid lines are invisible
+"	lineWeight = lineWeight * (lineWeight + ((1.0 - lineWeight) * GRID_WIDTH)) * GRID_INTENSITY;\n"
+	
+	// > Apply grid adjustments (phase 2)
+	//   - GRID_BIAS:
+	//        0.0: Use 'unbiased' lineWeight value calculated above
+	//        1.0: Scale lineWeight by current pixel luminosity
+	//             > i.e. the darker the pixel, the lower the intensity of the grid
+"	COMPAT_PRECISION float luma = (LUMA_R * color.r) + (LUMA_G * color.g) + (LUMA_B * color.b);\n"
+"	lineWeight = lineWeight * (luma + ((1.0 - luma) * (1.0 - GRID_BIAS)));\n"
+	
+	// Apply grid pattern
+	// (lineWeight == 1 -> set color to value specified by DARKEN_GRID)
+"	color.rgb = mix(color.rgb, vec3(1.0 - DARKEN_GRID), lineWeight);\n"
+
+	// Sample background texture and 'colorise' according to current pixel color
+	// (NB: the 'colorisation' here is lame, but the proper method is slow...)
+"	COMPAT_PRECISION vec3 bgTexture = vec3(0.9, 0.9, 0.9) * color.rgb;\n"
+	
+	// Blend current pixel with background according to luminosity
+	// (lighter color == more transparent, more visible background)
+	// Note: Have to calculate luminosity a second time... tiresome, but
+	// it's not a particulary expensive operation...
+"	luma = (LUMA_R * color.r) + (LUMA_G * color.g) + (LUMA_B * color.b);\n"
+"	color.rgb = mix(color.rgb, bgTexture.rgb, luma);\n"
+"	return color;\n"
 "}\n"
 
 "vec3 lcd_simple(vec3 color, vec3 tint)\n"
